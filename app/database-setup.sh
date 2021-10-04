192b4804d8931628e753108867765d3052452cd7
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-10-04 17:18:33 +0530

    add flask app for dockerizing

---
+++ app/database-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "starting mariadb in foreground"
+mysqld_safe &
+
+sleep 15s
+
+echo "setting up secure installation"
+mysql -e "SET PASSWORD FOR root@localhost = PASSWORD('s123456');FLUSH PRIVILEGES;"
+mysql -u root -ps123456 -e "DELETE FROM mysql.user WHERE User='';DELETE FROM mysql.user WHERE User='';DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+
+echo "Creating database"
+mysql -u root -ps123456 -e "CREATE DATABASE foo;CREATE USER 'abdullah'@'localhost' IDENTIFIED BY 'test123';GRANT ALL PRIVILEGES ON foo.* TO 'abdullah'@'localhost';"
+
+echo "Creating table in database"
+mysql -u abdullah -ptest123 -e "use foo;select database();create table students(id INT NOT NULL AUTO_INCREMENT,name VARCHAR(30) NOT NULL,email varchar(50) NOT NULL,phone varchar(10) NOT NULL,PRIMARY KEY(id));"
+
+
+echo "starting flask app"
+python3 __init__.py
