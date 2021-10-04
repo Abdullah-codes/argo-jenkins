0087cb8ba3abba65e41e196ed21691312086c4d7
Abdullah-codes <abdullahshaikhneo@gmail.com>
2021-10-04 19:17:52 +0530

    change  in script files 1

--- bash-script-login.sh
+++ bash-script-login.sh
@@ -12,7 +12,7 @@ cp docker-compose.yml.template docker-compose.yml
 
 sed -i "s/build/$BUILD_NUMBER/g" docker-compose.yml
 
-scp  -i $SSH_KEY init.sql docker-compose.yml ubuntu@13.233.245.60:/home/ubuntu
+scp  -i $SSH_KEY init.sql docker-compose.yml ubuntu@13.235.115.95:/home/ubuntu
 
 
 ssh -o StrictHostKeyChecking=no -i $SSH_KEY ubuntu@13.235.115.95 'bash -s' < ./bash-script-automation.sh
